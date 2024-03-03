99d3ccf161e1bcb4c089412284b95961489ec373
Shashank-21 <shashank@excelirate.net>
2024-03-03 13:54:11 +0530

    SSPA 15.4

---
+++ SSPA_15.4/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("homeBtn").addEventListener("click", () => {
+    document.getElementById("content").innerHTML =
+      "<h2>Home</h2><p>Welcome to our cloud kitchen. Enjoy our gourmet meals delivered right to your doorstep!</p>";
+  });
+
+  document.getElementById("productsBtn").addEventListener("click", () => {
+    fetch("products.html")
+      .then((response) => response.text())
+      .then((data) => (document.getElementById("content").innerHTML = data));
+  });
+
+  document.getElementById("testimonialsBtn").addEventListener("click", () => {
+    // Todo: Fetch the contents of the `testimonials.html` file, parse the text using `response.text()` function and set the innerHTML of the div with id `content` to the parsed data.
+  });
+});
